3910813a47faa2efdd9e63f2fd348fc6713140c1
Travis Swiers <tswiers@us.ci.org>
2023-03-15 11:10:33 -0600

    feat: connect and disconnect wallet

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,29 +1,53 @@
 import server from "./server";
 
 function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
+  async function connectWallet() {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      ethereum.on("accountsChanged", () => {disconnectWallet()});
+
+      const firstAccount = await ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => accounts[0]);
+      setAddress(firstAccount);
+
+      if (firstAccount) {
+        const {
+          data: { balance },
+        } = await server.get(`balance/${firstAccount}`);
+        setBalance(balance);
+      } else {
+        setBalance(0);
+      }
+
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  function disconnectWallet() {
+    setAddress("");
+    setBalance(0);
   }
 
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
+      {address ? (
+        <>
+          <label>
+            Wallet Address
+            <input disabled value={address}></input>
+          </label>
 
-      <div className="balance">Balance: {balance}</div>
+          <div className="balance">Balance: {balance}</div>
+        </>
+      ) : (<button className="button" onClick={connectWallet}>Connect</button>)}
     </div>
   );
 }
